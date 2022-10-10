20799f7845ae23b4823a62a6d05f5ccb48e63cc3
amlamanangekevin@gmail.com <amlamanangekevin@gmail.com>
2022-10-10 22:44:29 +0000

    attempt to migrate newsletter to SendStack #1

--- netlify/functions/submission-created.js
+++ netlify/functions/submission-created.js
@@ -7,17 +7,17 @@ exports.handler = async event => {
   const payload = JSON.parse(event.body).payload
   console.log(`Recieved a submission: ${payload.email}`)
 
-  return fetch("https://api.buttondown.email/v1/subscribers", {
+  return fetch("https://getsendstack.com/api/subscribers", {
     method: "POST",
     headers: {
-      Authorization: `Token ${BUTTONDOWN_API_KEY}`,
+      Authorization: `Bearer ${EMAIL_TOKEN}`,
       "Content-Type": "application/json",
     },
     body: JSON.stringify({ email: payload.email, notes: payload.name }),
   })
     .then(response => response.json())
     .then(data => {
-      console.log(`Submitted to Buttondown:\n ${data}`)
+      console.log(`Submitted to SendStack:\n ${data}`)
     })
     .catch(error => ({ statusCode: 422, body: String(error) }))
 } 
